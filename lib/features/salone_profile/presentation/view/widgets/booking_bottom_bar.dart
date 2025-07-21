a2f1be495a9d3b250d40698fb5da02b8b32caa6f
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2025-07-21 13:15:53 +0300

    Refactor UI in text field and booking bar

--- lib/features/salone_profile/presentation/view/widgets/booking_bottom_bar.dart
+++ lib/features/salone_profile/presentation/view/widgets/booking_bottom_bar.dart
@@ -41,7 +41,7 @@ class BookingBottomBar extends StatelessWidget {
                 Row(
                   children: [
                     Text(
-                      'المجيييموع',
+                      'المجموع',
                       style: getMediumStyle(
                         fontSize: FontSize.size14,
                         fontFamily: FontConstant.cairo,
